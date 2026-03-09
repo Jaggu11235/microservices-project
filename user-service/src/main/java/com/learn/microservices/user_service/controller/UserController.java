f70590dfa014e08b3d74a1bbf4acce575415b0ef
Jagadeesh R <jagadeesh.renuguntla@gmail.com>
2026-03-09 19:14:30 +0530

    User-Server

---
+++ user-service/src/main/java/com/learn/microservices/user_service/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.learn.microservices.user_service.controller;
+
+import com.learn.microservices.user_service.entity.User;
+import com.learn.microservices.user_service.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
---
+++ user-service/src/main/java/com/learn/microservices/user_service/service/UserService.java
@@ -0,0 +1,29 @@
+package com.learn.microservices.user_service.service;
+
+import com.learn.microservices.user_service.entity.User;
+import com.learn.microservices.user_service.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
